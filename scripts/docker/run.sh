5e2c535b4d1b5fe304471aeb90bdb9aba201e445
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-27 11:06:28 +0000

    update docker run script

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -7,13 +7,14 @@ set +o allexport
 CMD=${1:-/bin/bash}
 
 if [ "$HOST_WORKSPACE_ROOT" == "" ]; then
-    HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
-    echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
+  HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
+  echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
 fi
 
 docker run -it --rm \
   --runtime=nvidia \
   --gpus $NVIDIA_VISIBLE_DEVICES \
+  --network=$DOCKER_NETWORK \
   --ipc=$DOCKER_IPC \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
