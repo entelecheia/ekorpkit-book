dda87e55360eb9b9a8c7f306df05549fd37c2ead
Young Joon Lee <yj.lee@chu.ac.kr>
2022-08-01 12:16:07 +0000

    update start-notebook.sh

--- scripts/start-notebook.sh
+++ scripts/start-notebook.sh
@@ -9,4 +9,9 @@ DATESTAMP=$(date +'%y%m%d%H%M%S')
 LOGFILE=$JUPYTER_LOG_DIR/.jupyter-$DATESTAMP.log
 printf "Logs written to %s\n" "$LOGFILE"
 
-nohup jupyter lab --no-browser -NotebookApp.token=$JUPYTER_TOKEN --port=$JUPYTER_PORT --ip=0.0.0.0 --allow-root >$LOGFILE &
+nohup jupyter lab \
+    --no-browser \
+    -NotebookApp.token=$JUPYTER_TOKEN \
+    --port=$JUPYTER_PORT \
+    --ip=0.0.0.0 \
+    --allow-root >$LOGFILE &
