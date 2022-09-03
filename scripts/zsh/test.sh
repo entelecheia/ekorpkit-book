f03473fea37c7a3972bb11812107bfc76b4adb1e
entelecheia <yj.lee@chu.ac.kr>
2022-09-03 06:21:04 +0000

    feat: added zsh install test script

---
+++ scripts/zsh/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bash ./scripts/zsh/zsh-install.sh -d nodep \
+    -t jonathan \
+    -p git -p ssh-agent \
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions
