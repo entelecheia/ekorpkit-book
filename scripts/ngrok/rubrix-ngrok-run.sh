a11e8adc0ec405821a8799f6a38006ec608a04c3
entelecheia <yj.lee@chu.ac.kr>
2022-09-06 08:27:57 +0000

    updated esg usecase notebooks, support rubrix

---
+++ scripts/ngrok/rubrix-ngrok-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -o allexport
+source .env
+set +o allexport
+
+docker run -d -p $RUBRIX_NGROK_PORT \
+	--env NGROK_AUTH=$RUBRIX_NGROK_AUTH_TOKEN \
+	--env NGROK_PORT=http://$JUPYTER_HOST_IP:$RUBRIX_HOST_PORT \
+	--net $EKORPKIT_APP_DOCKER_NETWORK \
+	--name $RUBRIX_NGROK_CONTAINER_NAME \
+	wernight/ngrok:latest
