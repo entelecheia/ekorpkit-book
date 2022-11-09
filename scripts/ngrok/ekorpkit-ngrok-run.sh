d87e07397e939aa7a7bab3c88fdeb36a9ed2a337
entelecheia <yj.lee@chu.ac.kr>
2022-11-09 02:37:08 +0000

    updated aiart lectures

--- scripts/ngrok/ekorpkit-ngrok-run.sh
+++ scripts/ngrok/ekorpkit-ngrok-run.sh
@@ -5,9 +5,9 @@ source .env
 set +o allexport
 
 docker run -d -p $EKORPKIT_NGROK_PORT \
-	--env NGROK_AUTH=$NGROK_AUTH_TOKEN \
+	--env NGROK_AUTH=$EKORPKIT_BOOK_NGROK_AUTH_TOKEN \
 	--env NGROK_PORT=http://$JUPYTER_HOST_IP:$JUPYTER_HOST_PORT \
-	--link $EKORPKIT_DOCKER_CONTAINER_NAME:http \
+	--net $EKORPKIT_DOCKER_NETWORK_NAME \
 	--name $EKORPKIT_NGROK_CONTAINER_NAME \
 	wernight/ngrok:latest
 
